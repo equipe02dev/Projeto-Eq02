b91d2dd1a05e61a15e0ea01cecd82ba9bf02a066
Gabriel-Philipe <gabrielphilipe2601@gmail.com>
2025-03-30 13:22:47 -0300

    att

--- script2.js
+++
@@ -1,8 +0,0 @@
-function loadPage(page) {
-    fetch(/${page}/index.html)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('content').innerHTML = html;
-        })
-        .catch(error => console.error('Erro ao carregar a página:', error));
-}
